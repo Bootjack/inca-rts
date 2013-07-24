7ab7552f61235a04abc3ab7935cec010f1152977
Jason Hinebaugh <jhinebaugh@acs.org>
2013-07-24 12:38:43 -0400

    Started using Brackets as text editor, prompted by built-in JSLint to use strict and do some other tidying

--- public/src/game.js
+++ public/src/game.js
@@ -1,63 +1,69 @@
-require.config({
-    paths: {
-        crafty: 'lib/crafty/crafty-local',
-        jquery: 'lib/jquery/jquery',
-        modernizr: 'lib/modernizr/modernizr',
-        spine: 'lib/spine/lib/spine',
-        underscore: 'lib/underscore/underscore'      
-    }
-});
+var require, window;
 
-require(['modernizr', 'jquery', 'spine', 'underscore'], function () {
-    require(['crafty', 'src/config'], function (Crafty, config) {
-        //start Crafty
-        Crafty.init(800, 600);
-        Crafty.canvas.init();
-        window.Crafty = Crafty;
-      
-        //the loading screen - that will be display while assets loaded
-        Crafty.scene("loading", function () {
-            
-            Crafty.e('2D, HTML, Mouse')
-                .attr({x: 25, y: 25, w: 250, h:25})
-                .replace('<a href="#" class="scene-link">Resource Test</a>')
-                .bind('Click', function (e) {
-                    e.preventDefault();
-                    Crafty.scene('test-resource');
-                });
-
-            Crafty.e('2D, HTML, Mouse')
-                .attr({x: 25, y: 75, w: 250, h:25})
-                .replace('<a href="#" class="scene-link">Collector Test</a>')
-                .bind('Click', function (e) {
-                    e.preventDefault();
-                    Crafty.scene('test-collector');
-                });
-
-            Crafty.e('2D, HTML, Mouse')
-                .attr({x: 25, y: 125, w: 250, h:25})
-                .replace('<a href="#" class="scene-link">Processor Test</a>')
-                .bind('Click', function (e) {
-                    e.preventDefault();
-                    Crafty.scene('test-processor');
-                });
-        
-            //when everything is loaded, run the main scene
-            require([
-                "src/scenes/main",
-                'src/scenes/test-resource',
-                'src/scenes/test-collector',
-                'src/scenes/test-processor'
-            ], function () {     
-                if (config.scene != undefined) {
-                    Crafty.scene(config.scene);
-                }
-            });
-        
-        })
-                
-        //automatically play the loading scene
-        Crafty.scene("loading");
+(function () {
+    'use strict';
     
+    require.config({
+        paths: {
+            crafty: 'lib/crafty/crafty-local',
+            jquery: 'lib/jquery/jquery',
+            modernizr: 'lib/modernizr/modernizr',
+            spine: 'lib/spine/lib/spine',
+            underscore: 'lib/underscore/underscore'
+        }
     });
-});
+    
+    require(['modernizr', 'jquery', 'spine', 'underscore'], function () {
+        require(['crafty', 'src/config'], function (Crafty, config) {
+            //start Crafty
+            Crafty.init(800, 600);
+            Crafty.canvas.init();
+            window.Crafty = Crafty;
+          
+            //the loading screen - that will be display while assets loaded
+            Crafty.scene("loading", function () {
+                
+                Crafty.e('2D, HTML, Mouse')
+                    .attr({x: 25, y: 25, w: 250, h: 25})
+                    .replace('<a href="#" class="scene-link">Resource Test</a>')
+                    .bind('Click', function (e) {
+                        e.preventDefault();
+                        Crafty.scene('test-resource');
+                    });
+    
+                Crafty.e('2D, HTML, Mouse')
+                    .attr({x: 25, y: 75, w: 250, h: 25})
+                    .replace('<a href="#" class="scene-link">Collector Test</a>')
+                    .bind('Click', function (e) {
+                        e.preventDefault();
+                        Crafty.scene('test-collector');
+                    });
+    
+                Crafty.e('2D, HTML, Mouse')
+                    .attr({x: 25, y: 125, w: 250, h: 25})
+                    .replace('<a href="#" class="scene-link">Processor Test</a>')
+                    .bind('Click', function (e) {
+                        e.preventDefault();
+                        Crafty.scene('test-processor');
+                    });
+            
+                //when everything is loaded, run the main scene
+                require([
+                    "src/scenes/main",
+                    'src/scenes/test-resource',
+                    'src/scenes/test-collector',
+                    'src/scenes/test-processor'
+                ], function () {
+                    if (config.scene !== undefined) {
+                        Crafty.scene(config.scene);
+                    }
+                });
+            
+            });
+                    
+            //automatically play the loading scene
+            Crafty.scene("loading");
+        
+        });
+    });
+}());
--- public/src/scenes/test-collector.js
+++ public/src/scenes/test-collector.js
@@ -1,80 +1,91 @@
-Crafty.scene('test-collector', function() {
-    require([
-        'src/models/resource',
-        'src/controllers/resource-particle',
-        'src/controllers/resource-list',
-        
-        'src/models/collector',
-        'src/controllers/collector-bot'
-    ], function (
-        Resource, 
-        ResourceParticle, 
-        ResourceList,
-        
-        Collector,
-        CollectorBot
-    ) {
-        var resourceList, processorList, water, random, random2, x;
+var $, console, Crafty, require;
 
-        var log = console.log;
-        console.log = function (message) {
-            var line = $('<p>').html(message);
-            $('#spine-out').append(line);
-            log(message);
-        }
-
-        //  Testing continuous asynchronisity (gets really annoying...)
-        /*
-        x = 0;
-        Crafty.e('Delay').delay(function () {
-            console.log(x += 1);
-        }, 10, 100);
-        */
-  
-        //  Testing scaffold for resources
-  	    resourceList = new ResourceList({
-  	        el: $('#resource-list')
-  	    });
-  	    resourceList.render();
-        Resource.bind('create', function (model) {
-            var resourceParticle = new ResourceParticle({
-                el: $('<p>'),
-                model: model
+(function () {
+    'use strict';
+    
+    Crafty.scene('test-collector', function () {
+        require([
+            'src/models/resource',
+            'src/controllers/resource-particle',
+            'src/controllers/resource-list',
+            
+            'src/models/collector',
+            'src/controllers/collector-bot'
+        ], function (
+            Resource,
+            ResourceParticle,
+            ResourceList,
+            
+            Collector,
+            CollectorBot
+        ) {
+            var collectorBot, i, resourceList, processorList, water, random, random2, x;
+    
+            /*  This is quite an odd construction, but necessary to get V8 to accept log
+             *  as a function that duplicates console.log, retains its context, and yet
+             *  doesn't become recursive when console.log is redefined on the next few lines. 
+             *  This deserves some review as to whether it's really a good pattern. */
+            function log() {
+                return console.log.prototype;
+            }
+            console.log = function (message) {
+                var line = $('<p>').html(message);
+                $('#spine-out').append(line);
+                log(message);
+            };
+    
+            //  Testing continuous asynchronisity (gets really annoying...)
+            /*
+            x = 0;
+            Crafty.e('Delay').delay(function () {
+                console.log(x += 1);
+            }, 10, 100);
+            */
+      
+            //  Testing scaffold for resources
+            resourceList = new ResourceList({
+                el: $('#resource-list')
             });
-            $('#resource-map').append(resourceParticle.render().el);
-            resourceList.add(resourceParticle);
-        });
-        for (i = 0; i < 10; i += 1) {
-            random = Math.floor(10 - Math.random() * 20);
-            Resource.create({quantity: 100 + random, type: 'water'});
-        }
-        /*
-        Crafty.e('Delay').delay(function () {
-            resourceList.models[3].deplete(30);
-        }, 1000, 5);
-        */
-        
-        //  Testing scaffold for collectors
-        var collectorBot;
-        Collector.bind('create', function (model) {
-            collectorBot = new CollectorBot({
-                el: $('<p>'),
-                model: model
+            resourceList.render();
+            Resource.bind('create', function (model) {
+                var resourceParticle = new ResourceParticle({
+                    el: $('<p>'),
+                    model: model
+                });
+                $('#resource-map').append(resourceParticle.render().el);
+                resourceList.add(resourceParticle);
             });
-            $('#resource-map').append(collectorBot.render().el);
+            for (i = 0; i < 10; i += 1) {
+                random = Math.floor(10 - Math.random() * 20);
+                Resource.create({quantity: 100 + random, type: 'water'});
+            }
+            /*
+            Crafty.e('Delay').delay(function () {
+                resourceList.models[3].deplete(30);
+            }, 1000, 5);
+            */
+            
+            //  Testing scaffold for collectors
+            Collector.bind('create', function (model) {
+                collectorBot = new CollectorBot({
+                    el: $('<p>'),
+                    model: model
+                });
+                $('#resource-map').append(collectorBot.render().el);
+            });
+            Collector.create();
+    
+            resourceList.particles[0].offer(collectorBot, {
+                type: 'water',
+                quantity: 10
+            });
+                
+            /*
+            collectorBot.request(resourceList.particles[0], {
+                type: 'water',
+                quantity: 10
+            });
+            */
         });
-        Collector.create();
-
-        resourceList.particles[0].offer(collectorBot, {
-            type: 'water',
-            quantity: 10
-        });
-
-        /*
-        collectorBot.request(resourceList.particles[0], {
-            type: 'water',
-            quantity: 10
-        });
-        */
     });
-});
+}());
