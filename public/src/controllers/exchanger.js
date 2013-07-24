35f324893f2a700db0c84b267e520137eece2b80
Jason Hinebaugh <jhinebaugh@acs.org>
2013-07-23 20:11:54 -0400

    Completed to-date documentation of the Exchanger class and its associated Exchange model

--- public/src/controllers/exchanger.js
+++ public/src/controllers/exchanger.js
@@ -40,17 +40,24 @@ define(function () {
         
         
         negotiate: function (manifest) {
+        
             /*  Negotiation is the heart of the exchange logic. Or maybe the brain. This
              *  is where each side can set limits to what it can accept or provide in the
              *  transaction. Either side may set an opinion on the manifest to influence
-             *  the other party's behavior. If either side sets an opinion of "never", the
-             *  transaction is terminated immediately. */
-            var self = this;
+             *  the other party's behavior.
+             *
+             *  Two cases are handled before the negotiation is begun using the parent
+             *  controller's negotiate() method: Mutual acceptance of the manifest and
+             *  rejection by either party. */
+             
             if ('accept' === manifest.opinion && 'target' === this.role) {
+                //  If both parties accept, the transaction is carried out.             
                 this.peer.deliver(manifest, this.proxy(this.receive));
             } else if ('never' === manifest.opinion) {
+                //  If either party rejects, the transaction is terminated.             
                 this.close();
             } else {
+                //  Run some more complex negotiation logic based on what the parent thinks
                 manifest = this.parent.negotiate(manifest);
                 console.log('negotiating exchange as ' + this.role + ': ' + JSON.stringify(manifest));
                 this.peer.negotiate(manifest);
@@ -58,31 +65,40 @@ define(function () {
         },
         
         deliver: function (manifest, callback) {
-            //  Implement a deliver() method on the parent controller to handle this
-            console.log('delivering manifest from parent');
+        
+            /*  The Exchange instance merely forwards this to the parent's implementation
+             *  of a deliver() method, which will in turn run some game logic on its model.
+             *  Note that deliver will also pass a callback, which is the target's receive()
+             *  method proxied and forwarded from the target's final negotiate() call. */
+
+            console.log('delivering manifest');
             this.parent.deliver(manifest, callback);
             this.close();
         },
         
         receive: function (packet) {
-            //  Implement a receive() method on the parent controller to handle this
+
+            /*  The Exchange instance merely forwards this to the parent's implementation
+             *  of a receive() method, which will in turn run some game logic on its model. */
+
             console.log('receiving manifest');
             this.parent.receive(packet);
             this.close();
         }
     });
 
+
+    /*  The Exchanger class is what this AMD module actually returns. It is intended to
+     *  be added to an existing Spine.Controller to enable it to initiate and respond
+     *  to new exchange() transactions. */
+
     var Exchanger = Spine.Class.sub({
-        exchange: function (peer, role, manifest) {
-            this.exchanger = new Exchange({
-                peer: peer, 
-                role: role,
-                manifest: manifest,
-                parent: this
-            });
-            peer.open(this.exchanger);
-        },
         request: function (controller, manifest) {
+
+            /*  Initiates a new exchange with this controller taking the role of target.
+             *  This is one of two "public" methods used in the game logic to start 
+             *  a new transaction with another controller that includes Exchanger.*/
+
             this.exchanger = new Exchange({
                 peer: null, 
                 role: 'target',
@@ -92,6 +108,11 @@ define(function () {
             controller.exchange(this.exchanger, 'source', manifest);
         },
         offer: function (controller, manifest) {
+
+            /*  Initiates a new exchange with this controller taking the role of source.
+             *  This is one of two "public" methods used in the game logic to start 
+             *  a new transaction with another controller that includes Exchanger.*/
+
             this.exchanger = new Exchange({
                 peer: null, 
                 role: 'source',
@@ -100,18 +121,36 @@ define(function () {
             });
             controller.exchange(this.exchanger, 'target', manifest);
         },
+        
         negotiate: function (manifest) {
             //  Override this function!
             manifest.opinion = 'accept';
             return manifest;
         },
+        
         deliver: function () {
             //  Override this function!
             return {};        
         },
+        
         receive: function () {
             //  Override this function!
             return true;
+        },
+        
+        exchange: function (peer, role, manifest) {
+
+            /*  This method creates a new Exchange from this controller's side of the
+             *  transaction. This method will not need to be called from within the game
+             *  logic, it is only used by request() and offer(). */
+
+            this.exchanger = new Exchange({
+                peer: peer, 
+                role: role,
+                manifest: manifest,
+                parent: this
+            });
+            peer.open(this.exchanger);
         }
     });
     
